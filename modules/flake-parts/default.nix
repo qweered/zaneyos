33f7c5317e66ee3aebdb400cf707c4b144878d02
Aliaksandr <grubian2@gmail.com>
2025-05-30 00:32:45 +0300

    chore: less lines

--- modules/flake-parts/default.nix
+++ modules/flake-parts/default.nix
@@ -9,8 +9,6 @@
   perSystem =
     { ... }:
     {
-      # pkgsDirectory = ./pkgs;
-
       treefmt = {
         programs.nixfmt.enable = true;
         programs.nixfmt.width = 120;
@@ -21,30 +19,25 @@
   flake.nixosConfigurations.hyprnix = withSystem "x86_64-linux" (
     { system, ... }:
     let
-      users = inputs.self.moduleTree.users;
-      hosts = inputs.self.moduleTree.hosts;
-      systemModules = inputs.self.moduleTree.system;
+      moduleTree = inputs.self.moduleTree;
     in
     inputs.nixpkgs.lib.nixosSystem {
-      specialArgs = {
-        inherit inputs system;
-      };
-      modules = [
-        {
-          imports = (
-            users { qweered = true; }
-            ++ hosts { hyprnix = true; }
-            ++ systemModules {
-              impermanence = false;
-              virtualization.distrobox = false;
-            }
-          );
-          networking.hostName = "hyprnix";
-          system.stateVersion = "24.11";
-          nixpkgs.hostPlatform = system;
-          nixpkgs.overlays = with inputs; [ hyprpanel.overlay ];
+      specialArgs = { inherit inputs system; };
+      modules =
+        moduleTree.users { qweered = true; }
+        ++ moduleTree.hosts { hyprnix = true; }
+        ++ moduleTree.system {
+          impermanence = false;
+          virtualization.distrobox = false;
         }
-      ];
+        ++ [
+          {
+            networking.hostName = "hyprnix";
+            system.stateVersion = "24.11";
+            nixpkgs.hostPlatform = system;
+            nixpkgs.overlays = with inputs; [ hyprpanel.overlay ];
+          }
+        ];
     }
   );
 }
