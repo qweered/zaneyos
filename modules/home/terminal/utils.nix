860b3430a2215befcb5beffb3060435fec9e6a94
Aliaksandr <grubian2@gmail.com>
2025-06-12 00:00:31 +0300

    feat: add jq, azure-cli

--- modules/home/terminal/utils.nix
+++ modules/home/terminal/utils.nix
@@ -21,6 +21,10 @@
     bat # cat alternative
     yt-dlp # download videos
     lazydocker # docker in terminal
+
+    jq
+    azure-cli
+    azure-cli-extensions.application-insights
     # zathura imv # pdf/image viewer
     # wluma # automatic brightness control
     # unzip zip rar unrar # over 7z
