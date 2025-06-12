85f5c20528fdc2f91126e3d0b723c642c172e4f6
Aliaksandr <grubian2@gmail.com>
2025-06-12 05:02:03 +0300

    feat: nixcord.discord -> nixcord.vesktop

--- modules/home/apps/socials/discord.nix
+++ modules/home/apps/socials/discord.nix
@@ -3,6 +3,9 @@
   imports = [ inputs.nixcord.homeModules.nixcord ];
 
   # CONFIG
-  programs.nixcord.enable = true;
-
+  programs.nixcord = {
+    enable = true;
+    discord.enable = false;
+    vesktop.enable = true;
+  };
 }
