67340fd03bb1f3fc597ffcc83dff7299bd3cb354
Aliaksandr <grubian2@gmail.com>
2025-08-30 18:30:39 +0300

    neovim: use lazyvim for now

--- modules/home/devtools/neovim.nix
+++ modules/home/devtools/neovim.nix
@@ -1,8 +1,13 @@
 { inputs, ... }:
 
 {
+  imports = [ inputs.lazyvim.homeManagerModules.default ];
 
-  imports = [ inputs.nvf.homeManagerModules.default ];
+  programs.lazyvim = {
+    enable = true;
+  };
+
+  # imports = [ inputs.nvf.homeManagerModules.default ];
 
   # Neovim gui wrapper
   # programs.neovide = {
@@ -19,63 +24,63 @@
 
   # CONFIG: https://notashelf.github.io/nvf/options
   # Neovim config framework
-  programs.nvf = {
-    enable = true;
-    settings = {
-      vim = {
-        enableLuaLoader = true;
-        debugMode.enable = false;
-        # extraPackages = with pkgs; [ fzf ripgrep ];
-        assistant = {
-          # avante-nvim.enable = true;
-          copilot.enable = true;
-          copilot.cmp.enable = true;
-        };
-        # autogroups = {};
-        # autocomds = {};
-        autocomplete = {
-          enableSharedCmpSources = true; # with nvim-cmp
-          blink-cmp = {
-            enable = true;
-            friendly-snippets.enable = true;
-            # sourcePlugins = [];
-          };
-        };
-        autopairs.nvim-autopairs.enable = true;
-        binds = {
-          cheatsheet.enable = true;
-          hardtime-nvim.enable = true;
-          whichKey.enable = true;
-        };
-        clipboard = {
-          enable = true;
-          providers.wl-copy.enable = true;
-          registers = "unnamed,unnamedplus";
-        };
-        comments = {
-          comment-nvim.enable = true;
-        };
-        # dashboard = {};
-        # debugger = {};
-        diagnostics = {
-          # TODO
-          enable = true;
-          nvim-lint.enable = true;
-        };
-        filetree = {
-          neo-tree.enable = true; # or nvim-tree
-        };
-        formatter.conform-nvim.enable = true;
-        # fzf-lua.enable = true;
-        # gestures = {};
-        git = {
-          enable = true;
-          # git-conflict.enable = true;
-        };
-        lsp = {
-          enable = true;
-        };
-      };
-    };
-  };
+  # programs.nvf = {
+  #   enable = false;
+  #   settings = {
+  #     vim = {
+  #       enableLuaLoader = true;
+  #       debugMode.enable = false;
+  #       # extraPackages = with pkgs; [ fzf ripgrep ];
+  #       assistant = {
+  #         # avante-nvim.enable = true;
+  #         copilot.enable = true;
+  #         copilot.cmp.enable = true;
+  #       };
+  #       # autogroups = {};
+  #       # autocomds = {};
+  #       autocomplete = {
+  #         enableSharedCmpSources = true; # with nvim-cmp
+  #         blink-cmp = {
+  #           enable = true;
+  #           friendly-snippets.enable = true;
+  #           # sourcePlugins = [];
+  #         };
+  #       };
+  #       autopairs.nvim-autopairs.enable = true;
+  #       binds = {
+  #         cheatsheet.enable = true;
+  #         hardtime-nvim.enable = true;
+  #         whichKey.enable = true;
+  #       };
+  #       clipboard = {
+  #         enable = true;
+  #         providers.wl-copy.enable = true;
+  #         registers = "unnamed,unnamedplus";
+  #       };
+  #       comments = {
+  #         comment-nvim.enable = true;
+  #       };
+  #       # dashboard = {};
+  #       # debugger = {};
+  #       diagnostics = {
+  #         # TODO
+  #         enable = true;
+  #         nvim-lint.enable = true;
+  #       };
+  #       filetree = {
+  #         neo-tree.enable = true; # or nvim-tree
+  #       };
+  #       formatter.conform-nvim.enable = true;
+  #       # fzf-lua.enable = true;
+  #       # gestures = {};
+  #       git = {
+  #         enable = true;
+  #         # git-conflict.enable = true;
+  #       };
+  #       lsp = {
+  #         enable = true;
+  #       };
+  #     };
+  #   };
+  # };
 }
