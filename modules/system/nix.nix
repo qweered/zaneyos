519818497f71255f988fb171b1b967f23fe8eb3a
Aliaksandr <grubian2@gmail.com>
2025-09-22 02:19:11 +0300

    nix: disable nix-ld

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -37,11 +37,6 @@ in
     nix-init # init nix packages TODO: home-manager
   ];
 
-  programs.nix-ld = {
-    enable = true;
-    libraries = with pkgs; [ oxlint ];
-  };
-
   nix = {
     # TODO: should it be enabled?
     channel.enable = false;
@@ -76,18 +71,18 @@ in
 
       substituters = [
         "https://cache.nixos.org?priority=1" # lower number means higher priority
+        "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
         "https://cache.garnix.io"
         "https://chaotic-nyx.cachix.org"
-        "https://nix-community.cachix.org"
         "https://nix-gaming.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       ];
     };
