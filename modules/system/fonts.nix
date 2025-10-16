5fcbff31dd7563f7bcb1a24d899efdf71cd81b86
Aliaksandr <grubian2@gmail.com>
2025-10-16 11:39:06 +0300

    fonts: config

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -1,40 +1,38 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [ kdePackages.breeze-icons ];
+
   fonts = {
     enableDefaultPackages = false;
     packages = with pkgs; [
-      # icon fonts
-      material-symbols
-
-      # Sans(Serif) fonts
-      libertinus
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-color-emoji
-      roboto
-      (google-fonts.override { fonts = [ "Inter" ]; })
-
-      # monospace fonts
-      jetbrains-mono
-
-      # nerdfonts
       nerd-fonts.jetbrains-mono
+
+      material-symbols
       nerd-fonts.symbols-only
     ];
 
-    # user defined fonts
-    # the reason there's Noto Color Emoji everywhere is to override DejaVu's
-    # B&W emojis that would sometimes show instead of some Color emojis
-    fontconfig.defaultFonts =
-      let
-        addAll = lib.mapAttrs (_: v: v ++ [ "Noto Color Emoji" ]);
-      in
-      addAll {
-        serif = [ "Libertinus Serif" ];
-        sansSerif = [ "Inter" ];
-        monospace = [ "JetBrains Mono Nerd Font" ];
-        emoji = [ ];
+    fontconfig = {
+      allowBitmaps = false;
+      defaultFonts = {
+        serif = [
+          "Noto Serif"
+          "Noto Color Emoji"
+        ];
+        sansSerif = [
+          "Noto Sans"
+          "Noto Color Emoji"
+        ];
+        monospace = [
+          "JetBrains Mono Nerd Font"
+          "Noto Sans Mono"
+          "Noto Color Emoji"
+        ];
+        emoji = [ "Noto Color Emoji" ];
       };
+    };
   };
 }
