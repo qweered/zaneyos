41e5e20fcd8aa0ff6541ff3f625734feb3148915
Aliaksandr <grubian2@gmail.com>
2025-11-12 07:31:43 +0200

    office: test all of them

--- modules/home/apps/office.nix
+++ modules/home/apps/office.nix
@@ -2,5 +2,9 @@
 {
   # over libreoffice-qt-fresh (adds kde bloat), onlyoffice-desktopeditors
   # TODO: should be wpsoffice cause libreoffice adds 1.7 gb to storage
-  home.packages = [ pkgs.libreoffice-fresh ];
+  home.packages = [
+    pkgs.libreoffice-fresh
+    pkgs.wpsoffice
+    pkgs.onlyoffice-desktopeditors
+  ];
 }
