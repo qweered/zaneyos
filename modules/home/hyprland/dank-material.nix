7a1cc4c4f9a97b4384756c2967f2be1646a875f0
Aliaksandr <grubian2@gmail.com>
2025-11-12 13:03:45 +0200

    dank-material: init

---
+++ modules/home/hyprland/dank-material.nix
@@ -0,0 +1,19 @@
+{ inputs, ... }:
+{
+  imports = [ inputs.dankMaterialShell.homeModules.dankMaterialShell.default ];
+
+  programs.dankMaterialShell = {
+    enable = true;
+    systemd.enable = true; # Systemd service for auto-start
+    enableSystemMonitoring = true; # System monitoring widgets (dgop)
+    enableClipboard = true; # Clipboard history manager
+    enableVPN = true; # VPN management widget
+    enableBrightnessControl = true; # Backlight/brightness controls
+    enableColorPicker = true; # Color picker tool
+    enableDynamicTheming = true; # Wallpaper-based theming (matugen)
+    enableAudioWavelength = true; # Audio visualizer (cava)
+    enableCalendarEvents = true; # Calendar integration (khal)
+    enableSystemSound = true; # System sound effects
+  };
+
+}
