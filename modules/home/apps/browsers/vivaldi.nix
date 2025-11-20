2fe5e71a3a7f9352674112585abd902fcfa5398b
Aliaksandr <grubian2@gmail.com>
2025-11-20 21:48:19 +0200

    vivaldi: combine inherits

--- modules/home/apps/browsers/vivaldi.nix
+++ modules/home/apps/browsers/vivaldi.nix
@@ -3,10 +3,8 @@
   home.packages = with pkgs; [
     (vivaldi.override {
       enableWidevine = true;
-      inherit widevine-cdm;
-      # Proprietary codecs are broken now
-      # proprietaryCodecs = true;
-      # inherit vivaldi-ffmpeg-codecs;
+      proprietaryCodecs = true;
+      inherit widevine-cdm vivaldi-ffmpeg-codecs;
     })
   ];
 }
