a06f6cfb86a69cfd095991c160b6b0ae07b17065
Aliaksandr <grubian2@gmail.com>
2025-11-20 15:50:39 +0200

    cursor: increase size

--- modules/home/theme/cursor.nix
+++ modules/home/theme/cursor.nix
@@ -1,11 +1,10 @@
 { pkgs, ... }:
 
 {
-  # TODO: use hyprcursor instead, add HYPRCURSOR_SIZE, XCURSOR_SIZE to hyprland
   home.pointerCursor = {
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Ice";
-    size = 18;
+    size = 20;
     gtk.enable = true;
     x11.enable = true;
   };
