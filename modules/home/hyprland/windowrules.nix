58e2551272e826002817b035aaa014ff0c21a06e
Aliaksandr <grubian2@gmail.com>
2025-11-24 17:51:32 +0200

    windowrules: extend

--- modules/home/hyprland/windowrules.nix
+++ modules/home/hyprland/windowrules.nix
@@ -1,25 +1,23 @@
+let
+  fc = regex: "float, class:^(${regex})$";
+  ft = regex: "float, title:^(${regex})$";
+  fct = class: title: "float, class:^(${class})$, title:^(${title})$";
+in
 {
-  wayland.windowManager.hyprland.settings = {
-    windowrulev2 =
-      let
-        fc = regex: "float, class:^(${regex})$";
-        ft = regex: "float, title:^(${regex})$";
-        fct = class: title: "float, class:^(${class})$, title:^(${title})$";
-      in
-      [
-        (fc "xdg-desktop-portal")
-        (fc "com.saivert.pwvucontrol")
-        (fc "com.github.wwmm.easyeffects")
-        (fc "clipse")
-        (fc "io.github.kaii_lb.Overskride")
+  wayland.windowManager.hyprland.settings.windowrulev2 = [
+    (fc "xdg-desktop-portal")
+    (fc "com.saivert.pwvucontrol")
+    (fc "com.github.wwmm.easyeffects")
+    (fc "clipse.app")
+    (fc "io.github.kaii_lb.Overskride")
 
-        (fct "com.ayugram.desktop|io.github.kukuruzka165.materialgram" "Media viewer")
+    (fct "com.ayugram.desktop|io.github.kukuruzka165.materialgram" "Media viewer")
+    (fct "thunar" "File Operation Progress")
 
-        (ft "Picture-in-Picture")
-        (ft "Bitwarden - Vivaldi")
+    (ft "Picture-in-Picture")
+    (ft "Bitwarden - Vivaldi")
 
-        "suppressevent maximize, class:.*" # ignore maximizing requests from apps
-        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0" # fix dragging issues with XWayland
-      ];
-  };
+    "suppressevent maximize, class:.*" # ignore maximizing requests from apps
+    "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0" # fixes dragging issues with XWayland
+  ];
 }
