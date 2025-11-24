9cc91f4d9f9cad0d8d46fa7715b953d2cd280592
Aliaksandr <grubian2@gmail.com>
2025-11-24 12:51:13 +0200

    fonts: cleanup

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -2,36 +2,30 @@
 
 {
   fonts = {
-    enableDefaultPackages = false; # TODO:
+    enableDefaultPackages = false;
     packages = with pkgs; [
+      # icons
+      material-symbols
+
+      # sans-serif
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-color-emoji
-      jetbrains-mono # TODO: use correct name for jetbrains mono
-      nerd-fonts.jetbrains-mono
+      roboto
 
-      material-symbols
+      # monospace
+      jetbrains-mono
+
+      # nerd fonts
+      nerd-fonts.jetbrains-mono
       nerd-fonts.symbols-only
     ];
 
-    fontconfig = {
-      allowBitmaps = false;
-      defaultFonts = {
-        serif = [
-          "Noto Serif"
-          "Noto Color Emoji"
-        ];
-        sansSerif = [
-          "Noto Sans"
-          "Noto Color Emoji"
-        ];
-        monospace = [
-          "JetBrains Mono Nerd Font"
-          "Noto Sans Mono"
-          "Noto Color Emoji"
-        ];
-        emoji = [ "Noto Color Emoji" ];
-      };
+    fontconfig.defaultFonts = {
+      serif = [ "Noto Serif" ];
+      sansSerif = [ "Noto Sans" ];
+      monospace = [ "JetBrains Mono Nerd Font" ];
+      emoji = [ "Noto Color Emoji" ];
     };
   };
 }
