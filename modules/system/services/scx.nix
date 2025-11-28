6a00819d7b0166b84c76ea73d1d2cbe980429439
Aliaksandr <grubian2@gmail.com>
2025-11-28 16:08:23 +0200

    scx: use lavd instead of default

--- modules/system/services/scx.nix
+++ modules/system/services/scx.nix
@@ -1,3 +1,10 @@
+{ pkgs, ... }:
+
 {
-  services.scx.enable = true; # sched-ext scheduler
+  # sched-ext schedulers
+  services.scx = {
+    enable = true;
+    package = pkgs.scx.rustscheds;
+    scheduler = "scx_lavd";
+  };
 }
