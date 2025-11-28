82170c95b643e16fb96a01fc0e53dd2d6e442b44
Aliaksandr <grubian2@gmail.com>
2025-11-28 15:18:55 +0200

    man: disable my custom service

--- modules/system/debloat-modernize.nix
+++ modules/system/debloat-modernize.nix
@@ -1,9 +1,4 @@
-{
-  lib,
-  pkgs,
-  config,
-  ...
-}:
+{ lib, pkgs, ... }:
 
 {
   # Perlless https://github.com/NixOS/nixpkgs/blob/0260f927b7c1578b5c7cdefd7db7b660565cd362/nixos/modules/profiles/perlless.nix
@@ -39,70 +34,72 @@
 
   # Man-db cache generation cause it's very slow for fish
   # Stolen from https://github.com/MidAutumnMoon/TaysiTsuki/blob/025509df115b5d589f10d5c0d6d11d56ae74c4a2/nixos/documentation/module.nix
-  documentation.man.generateCaches = false;
-  systemd.services."man-db" = {
-    requires = [ "sysinit-reactivation.target" ];
-    after = [ "sysinit-reactivation.target" ];
-    partOf = [ "sysinit-reactivation.target" ];
-    wantedBy = [ "default.target" ];
-    path = [
-      config.documentation.man.man-db.package
-      pkgs.gawk
-    ];
-    serviceConfig = {
-      Nice = 19;
-      IOSchedulingClass = "idle";
-      IOSchedulingPriority = 7;
+  /*
+    documentation.man.generateCaches = false;
+    systemd.services."man-db" = {
+      requires = [ "sysinit-reactivation.target" ];
+      after = [ "sysinit-reactivation.target" ];
+      partOf = [ "sysinit-reactivation.target" ];
+      wantedBy = [ "default.target" ];
+      path = [
+        config.documentation.man.man-db.package
+        pkgs.gawk
+      ];
+      serviceConfig = {
+        Nice = 19;
+        IOSchedulingClass = "idle";
+        IOSchedulingPriority = 7;
+      };
+
+      serviceConfig.ExecStart = pkgs.writers.writeFish "mandbsvc" ''
+        set -l SystemManLoc "/run/current-system/sw/share/man"
+        set -l ContentRecord "/var/cache/man/nixos/man-db-state"
+
+        if [ ! -d "/var/cache/man/nixos" ]
+            mkdir -pv "/var/cache/man/nixos" || exit 1
+        end
+
+        if [ ! -f "$ContentRecord" ]
+            touch "$ContentRecord" || exit 1
+        end
+
+        set -l hashes "$(
+            find -L "$SystemManLoc" -type f -iname "*.gz" \
+                -exec sha256sum "{}" "+" \
+            | awk '{ print $1 }'
+            or exit 1
+        )"
+
+        set -l ultimate_hash (
+            echo $hashes \
+            | sort \
+            | string join "" \
+            | sha256sum - \
+            | awk '{ print $1 }'
+            or exit 1
+        )
+
+        set -l old_hash "$( string collect < "$ContentRecord" )"
+
+        echo "Old hash: $old_hash"
+        echo "New hash: $ultimate_hash"
+
+        if [ "$old_hash" != "$ultimate_hash" ]
+            echo "Hash changed, do a full man-db rebuild"
+            mandb -psc || exit 1
+            echo "Write new hash"
+            echo "$ultimate_hash" > "$ContentRecord"
+        else
+            echo "Hash not changed, skip"
+        end
+      '';
     };
 
-    serviceConfig.ExecStart = pkgs.writers.writeFish "mandbsvc" ''
-      set -l SystemManLoc "/run/current-system/sw/share/man"
-      set -l ContentRecord "/var/cache/man/nixos/man-db-state"
-
-      if [ ! -d "/var/cache/man/nixos" ]
-          mkdir -pv "/var/cache/man/nixos" || exit 1
-      end
-
-      if [ ! -f "$ContentRecord" ]
-          touch "$ContentRecord" || exit 1
-      end
-
-      set -l hashes "$(
-          find -L "$SystemManLoc" -type f -iname "*.gz" \
-              -exec sha256sum "{}" "+" \
-          | awk '{ print $1 }'
-          or exit 1
-      )"
-
-      set -l ultimate_hash (
-          echo $hashes \
-          | sort \
-          | string join "" \
-          | sha256sum - \
-          | awk '{ print $1 }'
-          or exit 1
-      )
-
-      set -l old_hash "$( string collect < "$ContentRecord" )"
-
-      echo "Old hash: $old_hash"
-      echo "New hash: $ultimate_hash"
-
-      if [ "$old_hash" != "$ultimate_hash" ]
-          echo "Hash changed, do a full man-db rebuild"
-          mandb -psc || exit 1
-          echo "Write new hash"
-          echo "$ultimate_hash" > "$ContentRecord"
-      else
-          echo "Hash not changed, skip"
-      end
-    '';
-  };
-
-  environment.extraSetup = ''
-    find "$out/share/man" \
-        -mindepth 1 -maxdepth 1 \
-        -not -name "man[1-8]" \
-        -exec rm -r "{}" ";"
-  '';
+    environment.extraSetup = ''
+      find "$out/share/man" \
+          -mindepth 1 -maxdepth 1 \
+          -not -name "man[1-8]" \
+          -exec rm -r "{}" ";"
+    ''
+  */
 }
